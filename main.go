73c880b6d8298a56f5b5dcbdc1cbc11cd72ebf7d
Alexander Sisco <36649949+alexandersisco@users.noreply.github.com>
2025-06-11 12:42:18 -0700

    add support for parsing json input

--- main.go
+++ main.go
@@ -10,12 +10,22 @@ import (
 
 func main() {
 	var args struct {
+		Json    string            `arg:"-j,--json"`
 		Strings map[string]string `arg:"-s,--string"`
 		Numbers map[string]int    `arg:"-n,--number"`
 		Bool    map[string]bool   `arg:"-b,--bool"`
 	}
 
 	arg.MustParse(&args)
+
+	if args.Json != "" {
+		var dat map[string]any
+		dat = FromJson(args.Json)
+
+		jsonStr := ToJson(dat)
+		fmt.Println(jsonStr)
+		args.Json = ""
+	}
 
 	jsonStr := ToJson(args)
 	fmt.Println(jsonStr)
@@ -27,4 +37,15 @@ func ToJson(data any) string {
 		log.Fatal(err)
 	}
 	return string(jsonBytes)
+}
+
+func FromJson(jsonStr string) map[string]any {
+	byt := []byte(jsonStr)
+	var dat map[string]any
+
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		panic(err)
+	}
+
+	return dat
 }
