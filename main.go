cdc4a082e4bea8533de3a0c0a7d08225f2e8f602
Alexander Sisco <36649949+alexandersisco@users.noreply.github.com>
2025-06-11 11:46:31 -0700

    convert args to json

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 
 	"github.com/alexflint/go-arg"
 )
@@ -12,5 +14,15 @@ func main() {
 	}
 
 	arg.MustParse(&args)
-	fmt.Println(args)
+
+	jsonStr := ToJson(args)
+	fmt.Println(jsonStr)
+}
+
+func ToJson(data any) string {
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(jsonBytes)
 }
